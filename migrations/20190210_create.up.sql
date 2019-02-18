85250cc0a7afbceb088de5aa1a841d2ccf70f1c9
Royall Spence <royall@royall.us>
2019-02-17 22:39:03 -0500

    Add obvious default value for body

--- migrations/20190210_create.up.sql
+++ migrations/20190210_create.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE todo (
   todo_id INTEGER PRIMARY KEY AUTO_INCREMENT,
   user_id CHAR(12) NOT NULL,
   date_added DATETIME DEFAULT NOW(),
-  body TEXT NOT NULL
+  body TEXT NOT NULL DEFAULT 'default'
 );
