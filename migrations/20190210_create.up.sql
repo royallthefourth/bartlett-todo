f1d134dafe0d8007091252fc661badfcf82f8786
Royall Spence <royall@royall.us>
2019-02-18 09:44:00 -0500

    Downgrade database to 10.1

--- migrations/20190210_create.up.sql
+++ migrations/20190210_create.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE todo (
   todo_id INTEGER PRIMARY KEY AUTO_INCREMENT,
   user_id CHAR(12) NOT NULL,
   date_added DATETIME DEFAULT NOW(),
-  body TEXT
+  body TEXT NOT NULL,
+  INDEX (date_added)
 );
