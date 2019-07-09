83dc5b46938f98e2242c3aa3b350a43fc4045ad7
Royall Spence <royall@royall.us>
2019-07-08 20:08:29 -0400

    Start migrating to Svelte

---
+++ js/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	// TODO fetch set of items
+	const items = []
+</script>
+
+{#each items as item}
+<TodoItem id={item.id} body={item.body} edit=false />
+{/each}
---
+++ js/TodoItem.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let body = '';
+    export let id = '';
+    export let edit = false;
+</script>
+
+{#if edit}
+<form>
+    <input type="text" required value={body}>
+    <button>Save</button>
+</form>
+{:else}
+<div>
+    <span>{body}</span>
+    <button>X</button>
+</div>
+{/if}
+
+<form>
+    <input type="text" required placeholder="Add a todo">
+    <button>Add</button>
+</form>
