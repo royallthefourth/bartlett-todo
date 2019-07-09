4c74307629dd1471477c844f2ae5ea7ab6bcde7d
Royall Spence <royall@royall.us>
2019-07-08 20:16:27 -0400

    Add NVM

--- build.sh
+++ build.sh
@@ -2,6 +2,14 @@
 
 set -e
 go build .
+
+unset NPM_CONFIG_PREFIX
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | dash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm current
+nvm install 10.15
+
 npm install
 npm run build
 gzip -kf static/*
