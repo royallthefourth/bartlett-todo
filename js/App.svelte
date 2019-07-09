06e859c1aea89421c832c61e0313f2ed475efe4f
Royall Spence <royall@royall.us>
2019-07-08 21:21:31 -0400

    Add behavior for new item post

--- js/App.svelte
+++ js/App.svelte
@@ -1,19 +1,32 @@
 <script>
-    import TodoItem from './TodoItem.svelte';
     import {onMount} from 'svelte';
 	let items = [];
+	let newBody = '';
 
     onMount(async () => {
         const res = await fetch(`/api/todo?order=date_added.asc`);
         items = await res.json();
     });
+    
+    async function addItem() {
+      let p = await fetch(`/api/todo`, {
+        credentials: 'same-origin',
+        method: 'POST',
+        body: JSON.stringify([{body: newBody}])
+      });
+      await p.json();
+
+      let res = await fetch(`/api/todo?order=date_added.asc`);
+      items = await res.json();
+      newBody = '';
+    }
 </script>
 
 {#each items as item}
 <TodoItem id={item.id} body={item.body} edit=false />
 {/each}
 
-<form>
-    <input type="text" required placeholder="Add a todo">
+<form on:submit|preventDefault={addItem}>
+    <input type="text" required placeholder="Add a todo" bind:value={newBody}>
     <button>Add</button>
 </form>
--- js/TodoItem.svelte
+++ js/TodoItem.svelte
@@ -2,16 +2,30 @@
     export let body = '';
     export let id = '';
     export let edit = false;
+
+    function enableEdit() {
+      edit = true
+    }
+
+    function saveItem() {
+      edit = false
+      // TODO patch item body back to server
+    }
+
+    function deleteItem() {
+      // TODO send delete request
+      // TODO delete this element?
+    }
 </script>
 
 {#if edit}
-<form>
-    <input type="text" required value={body}>
+<form on:submit|preventDefault={saveItem}>
+    <input type="text" required bind:value={body}>
     <button>Save</button>
 </form>
 {:else}
 <div>
-    <span>{body}</span>
-    <button>X</button>
+    <span on:click={enableEdit}>{body}</span>
+    <button on:click={deleteItem}>X</button>
 </div>
 {/if}
